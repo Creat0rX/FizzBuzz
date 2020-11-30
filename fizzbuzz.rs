017346b0415cc26ea4cd77048d39fd1498f9d2b4
ahartley42 <ameen.hartley@gmail.com>
2020-11-30 05:08:54 +0200

    added rust version

---
+++ fizzbuzz.rs
@@ -0,0 +1,22 @@
+fn main()
+{
+    for x in 1..101
+    {
+        if x % 15 == 0
+        {
+            println!("FizzBuzz");
+        }
+        else if x % 3 == 0
+        {
+            println!("Fizz");
+        }
+        else if x % 5 == 0
+        {
+            println!("Buzz");
+        }
+        else
+        {
+            println!("{}", x);
+        }
+    }
+}
